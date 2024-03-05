07750a9cff7084217b55dc06469d031a6236a802
Sophia Rynes <sarynes@wisc.edu>
2024-03-05 12:17:18 -0600

    final mean.sh

--- mean.sh
+++ mean.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-usage() {
-    echo "usage: ./mean.sh <column> [file.csv]" 1>&2
-    exit 1
-}
-
-if [[$# -lt 1 || $# -gt 2]]; then
-    usage
+if [[ $# -ne 2 ]]; then 
+    echo "usage: $0 <column> [file.csv]" 1>&2 
+    exit 0
 fi
+
 
 column="$1"
 file="$2"
@@ -18,7 +15,7 @@ if [ -z "$file" ]; then
 fi
 
 
-mean=$(cut -d ',' -f "$column" "$file" | tail -n +2 | { sum=0; count=0; while read value; do sum=$((sum + value)); ((count++)); done; echo "$sum / $count" | bc -l )
+mean=$(cut -d ',' -f "$column" "$file" | tail -n +2 | { sum=0; count=0; while read value; do sum=$(echo "scale=6; $sum + $value" | bc);  count=$(($count+1)); done; echo "scale=6; $sum / $count" | bc ; } )
 
 echo "Mean of column $column: $mean"
 
